ebb0e0f3dc52d77d17e5afd20e9d777b673042fd
jotas <jotasenator@gmail.com>
2021-08-01 20:43:12 -0400

    testing fizzBuzz function formating

--- src/FIZZBUZZCODE/fizzBuzz.js
+++ src/FIZZBUZZCODE/fizzBuzz.js
@@ -12,5 +12,4 @@ export const fizzBuzz = (i) => {
         }
     }
 
-
 }
--- src/FIZZBUZZCODE/fizzBuzz.test.js
+++ src/FIZZBUZZCODE/fizzBuzz.test.js
@@ -1,33 +1,20 @@
 
 import { fizzBuzz } from "./fizzBuzz"
 
-
 describe('FIZZBUZZ test', () => {
-
     it('with i=3', () => {
-
         expect(fizzBuzz(3)).toBe("Fizz")
-
-
     })
     it('with i=5', () => {
-
         expect(fizzBuzz(5)).toBe("Buzz")
-
-
     })
-
     it('with i=7', () => {
         expect(fizzBuzz(7)).toBe("not Fizz nor Buzz or FizzBuzz")
     })
-
     it('with i=90', () => {
-
         expect(fizzBuzz(90)).toBe("FizzBuzz")
     })
-
     it('with i=0', () => {
-
         expect(fizzBuzz(0)).toBe(undefined)
     })
 })
