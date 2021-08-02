4d7f78fb0a241e099668ae3355c5383deba27f94
jotas <jotasenator@gmail.com>
2021-08-01 20:38:14 -0400

    testing fizzBuzz function

---
+++ src/FIZZBUZZCODE/fizzBuzz.js
@@ -0,0 +1,16 @@
+export const fizzBuzz = (i) => {
+    if (i > 0) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            return 'FizzBuzz'
+        } else if (i % 3 === 0) {
+            return 'Fizz'
+        } else if (i % 5 === 0) {
+            return 'Buzz'
+        } else {
+            return 'not Fizz nor Buzz or FizzBuzz'
+        }
+    }
+
+
+}
